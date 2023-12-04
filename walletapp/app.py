e70addf02e927ca885adc202972c90678ca5f04c
QI SHEN <qishenki@163.com>
2023-12-04 17:38:06 +0800

    First try - uploading code

---
+++ walletapp/app.py
@@ -0,0 +1,18 @@
+safe_mode = True
+def test01():
+    a = 5
+    # print(s)
+
+def test02():
+    c1 = 3
+    c2 = 3
+    code = str(c1) + str(c2)
+    print(code)
+
+# print("A")
+
+def test03():
+    s1 = '10'
+    s2 = '21'
+    sum = int(s1) + int(s2)
+    print(sum)
